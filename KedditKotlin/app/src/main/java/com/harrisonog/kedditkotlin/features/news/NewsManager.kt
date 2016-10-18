edcce563e70b7853fffdde9d707dcaa313e6f8f6
Harrison Oglesby <hoglesby@klinche.com>
2016-10-18 15:10:06 -0700

    pt 6: restAPI linked to NewsManager

--- KedditKotlin/app/src/main/java/com/harrisonog/kedditkotlin/features/news/NewsManager.kt
+++ KedditKotlin/app/src/main/java/com/harrisonog/kedditkotlin/features/news/NewsManager.kt
@@ -1,6 +1,9 @@
 package com.harrisonog.kedditkotlin.features.news
 
+
 import com.harrisonog.kedditkotlin.commons.RedditNewsItem
+import com.harrisonog.kedditkotlin.api.RestAPI
+
 import rx.Observable
 
 /**
@@ -8,24 +11,39 @@ import rx.Observable
  *
  * Created by harrisonoglesby on 10/17/16.
  */
-class NewsManager() {
+class NewsManager(private val api: RestAPI = RestAPI()) {
 
-    fun getNews(): Observable<List<RedditNewsItem>> {
+    fun getNews(limit: String = "10"): Observable<List<RedditNewsItem>> {
         return Observable.create {
             subscriber ->
 
-            val news = mutableListOf<RedditNewsItem>()
-            for (i in 1..10){
-                news.add(RedditNewsItem(
-                        "author$i",
-                        "Title $i",
-                        i,
-                        1457207701L - i * 200,
-                        "http://lorempixel.com/200/200/technics/$i", // image url
-                        "url"
-                ))
+            val callResponse = api.getNews("", limit)
+            val response = callResponse.execute()
+
+            if (response.isSuccessful) {
+                val news = response.body().data.children.map {
+                    val item = it.data
+                    RedditNewsItem(item.author, item.title, item.num_comments, item.created,
+                            item.thumbnail, item.url)
+                }
+                subscriber.onNext(news)
+                subscriber.onCompleted()
+            } else {
+                subscriber.onError(Throwable(response.message()))
             }
-            subscriber.onNext(news)
+
+//            val news = mutableListOf<RedditNewsItem>()
+//            for (i in 1..10){
+//                news.add(RedditNewsItem(
+//                        "author$i",
+//                        "Title $i",
+//                        i,
+//                        1457207701L - i * 200,
+//                        "http://lorempixel.com/200/200/technics/$i", // image url
+//                        "url"
+//                ))
+//            }
+//            subscriber.onNext(news)
 
         }
     }
